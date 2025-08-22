8346f6afe8da3957cab8c69a4984d03a51da9ce5
[didicomingyaya] <[2820691747@qq.com]>
2025-08-22 18:05:18 +0800

    element主题色定制

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' }),],
+    }),
+    // 代码中可直接使用 ElMessage.success()，而无需写 import { ElMessage } from 'element-plus'。
+    // 模板中可直接使用 <el-button>，无需写 import { ElButton } from 'element-plus' 和 components: { ElButton }。
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
+    }
+  }
 })
