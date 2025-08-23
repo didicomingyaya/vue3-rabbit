e93af823d12b08d6346722cdcf0552d3601ee6c1
[didicomingyaya] <[2820691747@qq.com]>
2025-08-23 18:00:43 +0800

    路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//createRouter 创建路由实例
+//createWebHistory 使用HTML5的历史记录模式
+
+
+import Login from '../views/Login/index.vue'
+import Home from '../views/Home/index.vue'
+import Category from '../views/Category/index.vue'
+import Layout from '../views/Layout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Layout',
+      component: Layout,
+      children: [
+        {
+          path: '', //默认子路由
+          name: 'Home',
+          component: Home
+        },
+        {
+          path: 'category',
+          name: 'Category',
+          component: Category
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'Login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: Login,
     },
   ],
 })
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+    <div> 
+    我是首页 
+    <!-- 二级路由出口 -->
+     <RouterView/>
+    </div>
+</template>
+
+<!-- <script>
+// 即使你没有写export，Vue loader也会自动处理
+// 实际上相当于：
+export default {
+  name: 'Home'
+}
+</script> -->
